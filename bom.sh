71b672a4bf3c7a662a8a89399604ff3b62414c6b
Paul Kulyk <paul.kulyk@gmail.com>
2014-04-20 16:12:43 -0600

    Added some automatic calculators

---
+++ bom.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+openscad -o output.png master_bedroom.scad 2>&1 | grep ECHO | sed 's/^.*ECHO\:\ //g' | sort | uniq -c | sed 's/\"//g'
+
+# Total length of 16 inch wide shelving
+echo "Total length of 16 inch wood"
+openscad -o output.png master_bedroom.scad 2>&1 | grep ECHO | sed 's/^.*ECHO\:\ //g' | sort | uniq -c | sed 's/\"//g' | grep 16 | sed 's/[a-zA-Z]//g' | awk '{print $1, "*",  $4 }' | bc  | paste -s -d + - | bc
+
+
+# Total length of rod
+echo "Total length of rod"
+openscad -o output.png master_bedroom.scad 2>&1 | grep ECHO | sed 's/^.*ECHO\:\ //g' | sort | uniq -c | sed 's/\"//g' | grep -i rod | sed 's/[a-zA-Z]//g' | awk '{print $1, "*",  $4 }' | bc  | paste -s -d + - | bc
--- shelf.scad
+++ shelf.scad
@@ -1,7 +1,7 @@
 include <closet_space.scad>
 
 sh_tot_height=80;
-sh_width=0.5;
+sh_width=0.75;
 sh_depth=16;
 sh_length=24;
 sh_brkt_height=2;
@@ -26,7 +26,7 @@ module shelves ()
 {
   num_shelves=5;
   //Individual shelves
-  for( i = [1:(num_shelves+1)] )
+  for( i = [1:(num_shelves)] )
   {
     shelf(height = i*sh_tot_height/(num_shelves+1));
     // Brackets
